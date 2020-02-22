c6de317a575cf98c3cac2e0e4def4836a97711bf
Ghost <17600079797@163.com>
2020-02-22 19:29:52 +0800

    骨架

---
+++ src/components/page/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import Schart from 'vue-schart';
+import bus from '../common/bus';
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+        };
+    },
+    components: {
+        Schart
+    },
+    computed: {
+        role() {
+            return this.name === 'admin' ? '超级管理员' : '普通用户';
+        }
+    },
+    created() {
+    },
+    methods: {
+    }
+};
+</script>
+
+
+<style scoped>
+</style>
